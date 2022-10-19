9ea2badef54cb585b4ac010a4486f5acf75fdf7a
Dinesh.A <dinesh.a@thundersoft.com>
2022-10-19 12:08:48 +0530

    19-10-2022

--- Data_structure/sorting/insertion_sort.c
+++ Data_structure/sorting/insertion_sort.c
@@ -1,7 +1,24 @@
 #include<stdio.h>
+int *insertion_sort(int *arr,int n)
+{
+	int i,j,temp;
+	for(i=1;i<n;i++)
+	{
+		temp = arr[i];
+		j=i-1;
+		while(j>=0&&arr[j]>temp)
+		{
+			arr[j+1]=arr[j];
+			j--;
+		}
+		arr[j+1]=temp;
+	}
+	return arr;
+}
 int main()
 {
 	int n;
+	int* (*fp)(int *,int) = insertion_sort;
 	printf("Enter the No.of array\n");
 	scanf("%d",&n);
 
@@ -13,16 +30,11 @@ int main()
 	printf("Printing array element\n");
 	for(i=0;i<n;i++)
 		printf("%d ",arr[i]);
+	
+	int *a = fp(arr,n);
 
-	for(i=1;i<n;++i)
-	{
-		temp = arr[i];
-		for(j=i;j>0&&arr[j-1]>temp;j--)
-			arr[j]=arr[j-1];
-		arr[j] = temp;
-	}
 	printf("\nAfter the sorting elements are\n");
 	for(i=0;i<n;i++)
-		printf("%d ",arr[i]);
+		printf("%d ",a[i]);
 	printf("\n");
 }
